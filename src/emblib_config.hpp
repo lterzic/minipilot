d4902128eecfcfbc9616c17a707c357be779338d
Luka Terzic <lukaterzicbg@gmail.com>
2025-05-06 00:25:41 +0200

    cmake cleanup and freertos config added

--- src/emblib_config.hpp
+++ src/emblib_config.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
+// Use emblib from a single namespace
 #define EMBLIB_UNNEST_NAMESPACES    1
-
-#define EMBLIB_RTOS_TICK_MILLIS     1
+// Select the RTOS backend
 #define EMBLIB_RTOS_USE_FREERTOS    1
-#define EMBLIB_RTOS_SUPPORT_NOTIFICATIONS 1
-
+// Select the math backend
 #define EMBLIB_MATH_USE_EIGEN       1
