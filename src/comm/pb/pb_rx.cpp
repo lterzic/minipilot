711a14a77f88a67971936a94bf2dd4036e4a18a9
Luka Terzic <lukaterzicbg@gmail.com>
2025-07-27 13:05:13 +0200

    mp proto update and rx comms added

---
+++ src/comm/pb/pb_rx.cpp
@@ -0,0 +1,49 @@
+#include "pb_rx.hpp"
+#include "task_config.hpp"
+#include "logging/log.hpp"
+#include <pb_decode.h>
+
+namespace mp {
+
+pb_rx::pb_rx(emblib::io::istream<char>& rx_dev) :
+    task("PB rx task", TASK_RECEIVER_PRIORITY, m_stack),
+    m_rx_dev(rx_dev)
+{}
+
+void pb_rx::run()
+{
+    while (true) {
+        char recv_buf[sizeof(pb_mp_UplinkMessage)];
+        pb_mp_UplinkMessage recv_msg = pb_mp_UplinkMessage_init_zero;
+
+        // Read status is negative in case the read operation failed, else
+        // it is the size of the received message in bytes
+        ssize_t read_status = -1;
+        auto read_cb = [this, &read_status](ssize_t status) {
+            read_status = status;
+            notify_from_isr();
+        };
+        
+        if (!m_rx_dev.read_async(recv_buf, sizeof(recv_buf), read_cb)) {
+            log_warning("Receiver read start fail!");
+            // Sleep to give time to the receiver to unblock
+            sleep(milliseconds_t(100));
+            continue;
+        }
+
+        // Wait infinitely for received data
+        wait_notification(milliseconds_t(-1));
+        if (read_status <= 0) {
+            log_error("Receiver read error!");
+            continue;
+        }
+
+        pb_istream_t buf_istream = pb_istream_from_buffer((const pb_byte_t*)recv_buf, read_status);
+        if (pb_decode(&buf_istream, pb_mp_UplinkMessage_fields, &recv_msg)) {
+            // TODO (Optional): Add buffering for parsed messages
+            m_handlers[recv_msg.which_payload](recv_msg);
+        }
+    }
+}
+
+}
---
+++ src/comm/pb/pb_rx.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "pb/mp/link.nanopb.h"
+#include <emblib/io/istream.hpp>
+#include <emblib/rtos/queue.hpp>
+#include <emblib/rtos/task.hpp>
+#include <etl/delegate.h>
+#include <etl/map.h>
+
+namespace mp {
+
+class pb_rx : public emblib::task {
+public:
+    using handler_t = etl::delegate<void( const pb_mp_UplinkMessage&)>;
+
+public:
+    explicit pb_rx(emblib::io::istream<char>& rx_dev) noexcept;
+
+    /**
+     * Set the handler for a certain type of received message
+     */
+    void set_handler(size_t payload_type, handler_t handler) noexcept
+    {
+        m_handlers[payload_type] = handler;
+    }
+
+private:
+    void run() noexcept override;
+
+private:
+    // Size of this map should be equal to number of payload types
+    etl::map<size_t, handler_t, 4> m_handlers;
+    // Serial data receive device
+    emblib::io::istream<char>& m_rx_dev;
+    // Task stack
+    emblib::task_stack_t<1024> m_stack;
+};
+
+}
