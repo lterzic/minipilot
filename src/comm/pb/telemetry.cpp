cdd0c5dfa0bb5bbc53367ed31ce5ae5795794705
Luka Terzic <lukaterzicbg@gmail.com>
2025-08-02 08:49:23 +0200

    removed pb_util

--- src/comm/pb/telemetry.cpp
+++ src/comm/pb/telemetry.cpp
@@ -1,8 +1,26 @@
 #include "telemetry.hpp"
 #include "mp_config.hpp"
-#include "util/pb_util.hpp"
+
+#define PB_SET(struct, field, value) pb_set(struct.field, struct.has_ ## field, value)
 
 namespace mp {
+
+static void pb_set(pb_mp_Vector3f& pb_vec, bool& pb_has_field, const vector3f& mp_vec)
+{
+    pb_vec.x = mp_vec(0);
+    pb_vec.y = mp_vec(1);
+    pb_vec.z = mp_vec(2);
+    pb_has_field = true;
+}
+
+static void pb_set(pb_mp_Vector4f& pb_vec, bool& pb_has_field, const vector4f& mp_vec)
+{
+    pb_vec.w = mp_vec(0);
+    pb_vec.x = mp_vec(1);
+    pb_vec.y = mp_vec(2);
+    pb_vec.z = mp_vec(3);
+    pb_has_field = true;
+}
 
 telemetry::telemetry(
     pb_tx& tx,
