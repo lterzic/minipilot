befa27a106ad4172caf48a61ac557b48f922c556
Luka Terzic <lukaterzicbg@gmail.com>
2025-10-22 14:21:23 +0200

    fix rtti and module name

--- tests/binds/ekf.cpp
+++ tests/binds/ekf.cpp
@@ -3,7 +3,7 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(ekf, m, py::mod_gil_not_used()) {
+PYBIND11_MODULE(mpbind, m, py::mod_gil_not_used()) {
     py::class_<mp::ekf>(m, "ekf")
         .def(py::init<const mp::model&>())
         .def("update", &mp::ekf::update);
